6e4493a4b02dcc3813ff716bb029a78f2cd65af7
root <yuxiang.tan@gmail.com>
2019-05-09 17:42:11 +0800

    update FPfilter.py parameter

--- FPfilter-v1/FPfilter.py
+++ FPfilter-v1/FPfilter.py
@@ -71,8 +71,7 @@ if __name__ == "__main__":
         log_error.write("Warning: The input file input_vcf does not exist in FPfilter.sh, exit.\n"); sys.exit(1)
         
     if not os.path.exists(FPfilter_path):
-        print (FPfilter_path)
-	print ("Warning: The path of FPfilter folder is not correct, exit.\n")
+        print ("Warning: The path of FPfilter folder is not correct, exit.\n")
         log_error.write("Warning: The path of FPfilter folder is not correct, exit.\n"); sys.exit(1)
         
     #step1
